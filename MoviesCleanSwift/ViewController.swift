24fc041dd652bf17287a07c7cc71da4aea85de8e
Anton Gutkin <anton.gutkin@gmail.com>
2022-01-12 16:12:14 +0200

    Initial commit

---
+++ MoviesCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesCleanSwift
+//
+//  Created by Anton Gutkin on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
